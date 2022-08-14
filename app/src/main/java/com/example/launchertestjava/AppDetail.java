0743e2e13ffc03072d4ea296b877cae0a8092964
Pheerie <piri.komp@gmail.com>
2022-08-14 09:38:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/launchertestjava/AppDetail.java
@@ -0,0 +1,11 @@
+package com.example.launchertestjava;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+    String type;
+}
+
---
+++ app/src/main/java/com/example/launchertestjava/AppsListActivity.java
@@ -0,0 +1,104 @@
+package com.example.launchertestjava;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppsListActivity extends Activity {
+
+    private ApplicationInfo applicationInfo;
+    private String type;
+    private PackageManager manager;
+    private List<AppDetail> apps;
+    private ListView list;
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+
+        loadApps();
+        loadListView();
+        addClickListener();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void loadApps() {
+        manager = getPackageManager();
+        apps = new ArrayList<>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for (ResolveInfo ri : availableActivities) {
+            AppDetail app = new AppDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+
+        try {
+            applicationInfo = manager.getApplicationInfo(availableActivities.get(0).activityInfo.packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadListView() {
+        list = (ListView) findViewById(R.id.apps_list);
+
+        ArrayAdapter<AppDetail> adapter = new ArrayAdapter<AppDetail>(this, R.layout.list_item, apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, null);
+                }
+
+                ImageView appIcon = (ImageView) convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).icon);
+
+                TextView appLabel = (TextView) convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).label);
+
+                ImageView appName = (ImageView) convertView.findViewById(R.id.icon_second);
+                appName.setImageDrawable(apps.get(position).icon);
+
+                return convertView;
+            }
+        };
+        list.setAdapter(adapter);
+    }
+
+    private void addClickListener() {
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(pos).name.toString());
+
+                AppsListActivity.this.startActivity(i);
+            }
+        });
+    }
+
+
+}
